f3feeb6adad1b4bbc820c6c1bc0673085f055e37
jb-park <ftstokpark@naver.com>
2018-10-21 17:07:07 +0900

    1476 Solved

---
+++ python/solved/1476.py
@@ -0,0 +1,16 @@
+E, S, M = map(int, input().split())
+
+RANGE_E, RANGE_S, RANGE_M = 15, 28, 19
+year = S
+if E == RANGE_E:
+    E = 0
+if M == RANGE_M:
+    M = 0
+while True:
+    
+
+    if (year % RANGE_E) == E and (year % RANGE_M) == M:
+        break
+    else:
+        year = year + RANGE_S
+print(year)
