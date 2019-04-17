d17ffb59427ccb931d444e780ce6aafbb7a9d87d
jaegoon <newenws@naver.com>
2019-04-18 00:50:43 +0900

    TILING2

---
+++ algospot/chap8/tiling2/jaehlee.java
@@ -0,0 +1,60 @@
+package gitStudy.acmicpc.algospot.chap8.tiling2;
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+/*
+ */
+
+public class jaehlee {
+ 
+	static int N;	
+	static int TC;
+	static int answer;
+	static Scanner sc;
+	static int dp[];
+	
+	static void init() {
+		
+		N = sc.nextInt();
+		answer = 0;
+		dp = new int[N+1];
+	}
+	
+	static void solve() {
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		if(N>=2) dp[2] = 2;
+		
+		for(int i=3;i<=N;i++) {
+			dp[i] = (dp[i-1]+dp[i-2])%1000000007;
+		}
+		
+		answer = dp[N];
+	}
+	
+	public static void main(String [] args) throws Exception {
+		
+		sc = new Scanner(System.in);
+		TC = sc.nextInt();
+		
+		for(int t=1; t<=TC; t++) {
+			
+			init();
+			
+			solve();
+			
+			System.out.println(answer);
+		}
+		
+		
+		
+		sc.close();
+	}
+
+}
+
+
+
